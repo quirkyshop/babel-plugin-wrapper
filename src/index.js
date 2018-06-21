8108f3d8df7b46a178c22082b3ab315b3f97b955
quirkyshop <quirkyvar@gmail.com>
2018-06-21 23:22:33 +0800

    feat: update

--- src/index.js
+++ src/index.js
@@ -9,26 +9,33 @@ function libNameFormat (name) {
     return "_lib_" + name.toLowerCase();
 }
 
+function requireLibNameFormat (name) {
+    return name.replace(/[A-Z]/g, (s1, s2) => {
+        return (s2 !== 0 ? '-' : '') + s1.toLowerCase()
+    });
+}
+
 function wrapperFormat (name) {
     return '_' + camel2Underline(name.replace(/\//g, '_'));
 }
 
-function wrapperResultFormat (name) {
-    return '_wrapper_result_' + name;
+function kindResultFormat (kind, name) {
+    return '_' + kind + '_result_' + name;
 }
 
 module.exports = function(babel) {
   var t = babel.types;
 
-  const wrapperPrefix = 'noform/dist/wrapper/';
+  const wrapperPrefix = 'noform/lib/wrapper/';
+  const dialogPrefix = 'noform/lib/dialog/';
+  const repeaterPrefix = 'noform/lib/repeater/';
   
   const importTraverseObject = {
     ExpressionStatement: function(path) {
         // debugger;
     },
     ImportDeclaration(path, data) {
-      const { opts = {}, runtimeData } = data;
-      path.skip()
+      const { opts = {}, runtimeData, wrapperData } = data;      
 
         var source = '';
         var target = [];
@@ -41,9 +48,19 @@ module.exports = function(babel) {
         // is noform wrapper 
         let specificParams = [];
         let specificRefs = [];
+        // fix multi entry
+        let rawParams = [];
+        let rawRefs = [];
+
+        if (wrapperData[source]) {
+            rawParams = wrapperData[source].params;
+            rawRefs = wrapperData[source].refs;
+        }
+
         if (source.startsWith(wrapperPrefix)) {
+            // eg. noform/dist/wrapper/antd
             const sourceKind = source.split(wrapperPrefix)[1];
-            const wrapperResult = wrapperResultFormat(sourceKind);
+            const kindResult = kindResultFormat('wrapper', sourceKind);
 
             if (Array.isArray(specifiersNode)) {
                 specifiersNode.forEach((item) => {
@@ -53,55 +70,206 @@ module.exports = function(babel) {
                     runtimeData[source][item.local.name] = { parent: path };
 
                     // paramters for wrapper
-                    const formatLibName = libNameFormat(item.local.name);
-                    specificParams.push(
-                        t.objectProperty(
-                            t.identifier(item.local.name),
-                            t.identifier(formatLibName),
-                        )
-                    );
-
-                    specificRefs.push(t.variableDeclaration('const', [
-                        t.variableDeclarator(
-                            t.identifier(item.local.name),
-                            t.memberExpression(
-                                t.identifier(wrapperResult),
-                                t.identifier(item.local.name)
-                            )
-                        )
-                    ]));
+                    // eg. { Button: _lib_button }
+                    if (rawParams.indexOf(item.local.name) === -1) {
+                        rawParams.push(item.local.name);
+                        rawRefs.push({ varName: item.local.name, source: kindResult });
+                    }
                   }
                 })
             }
             
-            path.replaceWithMultiple([
-                t.variableDeclaration('const', [
-                    t.variableDeclarator(
-                        t.identifier(wrapperResult),
-                        t.callExpression(
-                            t.identifier(wrapperFormat(source)),
-                            [t.objectExpression(specificParams)]
-                        )
-                    )
-                ])
-            ].concat(specificRefs));
+            path.remove(); // 清理
+            wrapperData[source] = {
+                varName: kindResult,
+                params: rawParams,
+                refs: rawRefs
+            };
+        } else if (source.startsWith(repeaterPrefix) || source.startsWith(dialogPrefix)) {
+            // eg. noform/dist/dialog/antd
+            let sourceKind = '';
+            let kindResult = '';
+
+            if (source.startsWith(repeaterPrefix)) {
+                sourceKind = source.split(repeaterPrefix)[1];
+                kindResult = kindResultFormat('repeater', sourceKind);
+            } else {
+                sourceKind = source.split(dialogPrefix)[1];
+                kindResult = kindResultFormat('dialog', sourceKind);
+            }
+            
+            ['Button', 'Input', 'Modal'].forEach((localName) => {
+                target.push(localName);
+                if (!runtimeData[source]) runtimeData[source] = {};
+                runtimeData[source][localName] = { parent: path };
+
+                // paramters for wrapper
+                // eg. { Button: _lib_button }
+                if (rawParams.indexOf(localName) === -1) {
+                    rawParams.push(localName);                    
+                }                
+              })
+
+            // eg. const _dialog_result_antd = _noform_dist_dialog_antd(...params)            
+            var originName = '';
+            if (path.node && path.node.specifiers && path.node.specifiers[0] &&
+                path.node.specifiers[0].local && path.node.specifiers[0].local.name) {
+                if (t.isImportDefaultSpecifier(path.node.specifiers[0])) {
+                    originName = path.node.specifiers[0].local.name;
+                }                
+            }
+
+            if (!originName) {
+                if (path.node && Array.isArray(path.node.specifiers)) {
+                    path.node.specifiers.map((specifierItem) => {
+                        if (specifierItem.local && specifierItem.local.name) {
+                            rawRefs.push({ varName: specifierItem.local.name, source: kindResult });
+                        }                        
+                    });
+                }
+            }
+
+            path.remove(); // 清理
+            wrapperData[source] = {
+                varName: originName || kindResult,
+                params: rawParams,
+                refs: rawRefs
+            };
+        } else {
+            path.skip();
         }
     }
+  }
+
+  function handleWrapper (file, wrapperData, opts = {}) {    
+    let weigthMap = {
+        repeater: null,        
+        dialog: null,
+        wrapper: null,
+    };
+
+    let dialogName = '';
+    let hasDialog = false;
+    let sortedKeys = [];
+
+    Object.keys(weigthMap).forEach((weightKey) => {
+        Object.keys(wrapperData).forEach((wrapperKey) => {
+            if (wrapperKey.indexOf(weightKey) !== -1) {
+                if (weightKey === 'dialog' && !hasDialog) {
+                    hasDialog = true;
+                    dialogName = wrapperData[wrapperKey].varName;
+                }
+                sortedKeys.push(wrapperKey)
+            }
+        });
+    });
+
+    sortedKeys.forEach((wrapperKey) => {
+        const currentWrapper = wrapperData[wrapperKey];
+        const { varName, params, refs } = currentWrapper;
+        let objParams = [];
+        let objRefs = [];
+        
+        if (params && Array.isArray(params)) {
+            params.forEach((paramName) => {
+                objParams.push(
+                    t.objectProperty(
+                        t.identifier(paramName),
+                        t.identifier(libNameFormat(paramName)),
+                    )
+                );
+            });
+        }
+
+        if (refs && Array.isArray(refs)) {
+            refs.forEach((refItem) => {
+                const { varName, source } = refItem;
+                objRefs.push(t.variableDeclaration('const', [
+                    t.variableDeclarator(
+                        t.identifier(varName),
+                        t.memberExpression(
+                            t.identifier(source),
+                            t.identifier(varName)
+                        )
+                    )
+                ]));
+            });
+        }
+
+        let dialogRef = [];
+        if (wrapperKey.startsWith(repeaterPrefix)) {
+            let dialogRefName = '';
+            if (!hasDialog) {
+                const ftDialogKey = wrapperKey.replace('repeater', 'dialog');
+                const sourceKind = wrapperKey.split(repeaterPrefix)[1];
+                dialogRefName = kindResultFormat('dialog', sourceKind);
+
+                dialogRef.push(t.variableDeclaration('const', [
+                    t.variableDeclarator(
+                        t.identifier(dialogRefName), // _wrapper_result_antd
+                        t.callExpression(
+                            t.identifier(wrapperFormat(ftDialogKey)), // noform/lib/wrapper/antd
+                            [t.objectExpression(objParams)]
+                        )
+                    )
+                ]));                
+            } else {
+                dialogRefName = dialogName;
+            }
+
+            objParams = [].concat(objParams, (
+                t.objectProperty(
+                    t.identifier('Dialog'),
+                    t.identifier(dialogRefName),
+                )
+            ));
+        }
+
+        const insertNodes = [].concat(
+            dialogRef,
+            [
+                t.variableDeclaration('const', [
+                    t.variableDeclarator(
+                        t.identifier(varName), // _wrapper_result_antd
+                        t.callExpression(
+                            t.identifier(wrapperFormat(wrapperKey)), // noform/lib/wrapper/antd
+                            [t.objectExpression(objParams)]
+                        )
+                    )
+                ]),
+            ],
+            objRefs
+        );
+
+        file.path.unshiftContainer("body", insertNodes);   
+    });
   }
 
   function handleAdd (file, runtimeData, opts = {}) {    
     const prefix = 'lib';
     const wrapperLibNames = Object.keys(runtimeData);
+    let registeredComponent = [];
     wrapperLibNames.forEach(name => {
         const libDeps = runtimeData[name];
-        const wrapperLibName = name.split(wrapperPrefix)[1];
+        let resultLibName = '';
+        if (name.startsWith(wrapperPrefix)) {
+            resultLibName = name.split(wrapperPrefix)[1];
+        } else if (name.startsWith(dialogPrefix)) {
+            resultLibName = name.split(dialogPrefix)[1];
+        }
+
+        // addDefault天然去重
         Object.keys(libDeps).forEach((libName) => {
-            const lowerLibName = libName.toLowerCase();
-            const sourcePath = `${wrapperLibName}/${prefix}/${lowerLibName}`;
-            addDefault(file.path, sourcePath, { nameHint: libNameFormat(libName) })
-            console.log('======*****===', sourcePath);
+            if (registeredComponent.indexOf(libName) === -1) {
+                registeredComponent.push(libName);
+                const lowerLibName = requireLibNameFormat(libName);
+                const sourcePath = `${resultLibName}/${prefix}/${lowerLibName}`;
+                // eg: import _lib_input from "antd/lib/input";
+                addDefault(file.path, sourcePath, { nameHint: libNameFormat(libName) })
+            }            
         });        
 
+        // eg: import _noform_lib_wrapper_antd from "noform/lib/wrapper/antd";
         addDefault(file.path, name, { nameHint: wrapperFormat(name) })
     });
   }
@@ -109,19 +277,23 @@ module.exports = function(babel) {
   return {
     pre(path) {
       this.runtimeData = {};
+      this.wrapperData = {};
     },
     visitor: {
       Program(path, data) {
         path.traverse(importTraverseObject, {
           opts: data.opts,
-          runtimeData: this.runtimeData
+          runtimeData: this.runtimeData,
+          wrapperData: this.wrapperData
         });
 
+        handleWrapper(data.file, this.wrapperData, data.opts)
         handleAdd(data.file, this.runtimeData, data.opts);
       }
     },
     post() {
-      delete this.runtimeData
+      delete this.runtimeData;
+      delete this.wrapperData;
     }    
   };
 };
